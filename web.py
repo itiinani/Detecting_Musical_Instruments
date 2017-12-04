53fe4bb6c48f5d6f9ddd10730ca67a8a8f44e7a6
itiinani <Nov16_11>
2017-12-04 13:33:21 -0800

    changes

--- web.py
+++ web.py
@@ -10,24 +10,31 @@ def hello_world():
 
     return render_template("/pages/charts/chartjs.html")
 
-@app.route("/PC")
-def parallel_coordinates_graph():
-    return render_template("/pages/charts/parallel_coordinates.html")
+@app.route("/results")
+def results():
+    return render_template("/pages/charts/results.html")
 
-@app.route("/FD")
-def forceDirected_graph():
-    return render_template("/pages/charts/forceDirected.html")
-
-@app.route("/AT")
-def allTranscripts_graph():
-    return render_template("/pages/charts/allCharts.html")
+@app.route("/cons_results")
+def cons_results():
+    return render_template("/pages/charts/consolidated_result.html")
 
 @app.route("/uploadmusicfile", methods=['GET','POST'])
 def uploadfile_and_detect():
-     fileDetection()
-     return render_template("/pages/charts/chartjs.html")
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'musicfile' not in request.files:
+           # flash('No file part')
+            return redirect(request.url)
+        file = request.files['musicfile']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            #flash('No selected file')
+            return redirect(request.url)
+        fileDetection(file.filename)
+    return render_template("/pages/charts/chartjs.html")
 
 
 
 if __name__ == "__main__":
-   app.run("127.0.0.1",3012)
+   app.run("127.0.0.1",3013)
